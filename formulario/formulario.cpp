852ab4caeddaad4f2057b738860f368b5448e0bf
CaioTuring <caioaagarbelini@gmail.com>
2022-12-08 17:00:30 -0300

    Arquivos da minha branch (caio)

---
+++ formulario/formulario.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "formulario.h"
+
+std::string Formulario::GetEmailCliente(){
+    return emailCliente;
+}
+
+std::string Formulario::GetNomeCliente(){
+    return nomeCliente;
+}
+
+std::string Formulario::GetTelClinte(){
+    return telCliente;
+}
