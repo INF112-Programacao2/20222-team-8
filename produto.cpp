dbc1bb77e1cc56bc35e148396cb35b0d654df8a6
CaioTuring <caioaagarbelini@gmail.com>
2022-12-09 16:47:25 -0300

    Adicionando arquivos txt especificos para exibição para clientes

--- produto.cpp
+++ produto.cpp
@@ -20,6 +20,11 @@ double Produto::getPrecoProduto(){
 
 void Produto::cadastraProduto(){
     bool saida = true;
+    std::ofstream moletomcliente("moletomcliente.txt", std::ios::app);
+    std::ofstream camisaCliente("camisaCliente.txt", std::ios::app);
+    std::ofstream canecaCliente("canecaCliente.txt", std::ios::app);
+    std::ofstream tiranteCliente("tiranteCliente.txt", std::ios::app);
+
     while(saida){
         int prod;
         std::cout << "Qual produto quer cadastrar 1 - Moletom | 2 - Camisa | 3 - Caneca | 4 - Tirante?: ";
@@ -30,15 +35,24 @@ void Produto::cadastraProduto(){
                 std::cin >> idProduto;
                 std::cout << "Entre com o preco do produto: ";
                 std::cin >> precoProduto;
+                moletomcliente << "***Moletom CCP***" << std::endl;
+                moletomcliente << std::endl;
+                moletomcliente << "Preco: R$" << precoProduto << std::endl;
+                moletomcliente << std::endl;
+                moletomcliente << "Tamanhos Disponiveis: ";
                 while(true){
                     char flag;
                     std::ofstream produto("moletom.txt", std::ios::app);
+                    
                     produto << "***Cadastrando Moletons***" << std::endl;
                     produto << "idProduto: " << idProduto << std::endl;
                     std::cout << "Entre com o tamanho do produto: ";
                     std::cin >> tamanhoProduto;
                     produto << "tamanhoProduto: " << tamanhoProduto << std::endl;
                     produto << "precoProduto: R$" << precoProduto << std::endl;
+
+                    moletomcliente << tamanhoProduto << " ";
+
                     std::cout << "Entre com a quantidade desse produto: ";
                     std::cin >> quantidade;
                     produto << quantidade << " unidades" << std::endl;
@@ -66,6 +80,11 @@ void Produto::cadastraProduto(){
                 std::cin >> idProduto;
                 std::cout << "Entre com o preco do produto: ";
                 std::cin >> precoProduto;
+                camisaCliente << "***Camisa CCP***" << std::endl;
+                camisaCliente << std::endl;
+                camisaCliente << "Preco: R$" << precoProduto << std::endl;
+                camisaCliente << std::endl;
+                camisaCliente << "Tamanhos Disponiveis: ";
                 while(true){
                     char flag;
                     std::ofstream produto("camisa.txt", std::ios::app);
@@ -75,6 +94,9 @@ void Produto::cadastraProduto(){
                     std::cin >> tamanhoProduto;
                     produto << "tamanhoProduto: " << tamanhoProduto << std::endl;
                     produto << "precoProduto: R$" << precoProduto << std::endl;
+
+                    camisaCliente << tamanhoProduto << " ";
+
                     std::cout << "Entre com a quantidade desse produto: ";
                     std::cin >> quantidade;
                     produto << quantidade << " unidades" << std::endl;
@@ -102,6 +124,11 @@ void Produto::cadastraProduto(){
                 std::cin >> idProduto;
                 std::cout << "Entre com o preco do produto: ";
                 std::cin >> precoProduto;
+                canecaCliente << "***Caneca CCP***" << std::endl;
+                canecaCliente << std::endl;
+                canecaCliente << "Preco: R$" << precoProduto << std::endl;
+                canecaCliente << std::endl;
+                canecaCliente << "Tamanho: U" << std::endl;
                 while(true){
                     char flag;
                     std::ofstream produto("caneca.txt", std::ios::app);
@@ -133,6 +160,11 @@ void Produto::cadastraProduto(){
                 std::cin >> idProduto;
                 std::cout << "Entre com o preco do produto: ";
                 std::cin >> precoProduto;
+                tiranteCliente << "***Tirante CCP***" << std::endl;
+                tiranteCliente << std::endl;
+                tiranteCliente << "Preco: R$" << precoProduto << std::endl;
+                tiranteCliente << std::endl;
+                tiranteCliente << "Tamanho: U" << std::endl;
                 while(true){
                     char flag;
                     std::ofstream produto("tirante.txt", std::ios::app);
