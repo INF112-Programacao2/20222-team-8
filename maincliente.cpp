142125691281bc0aebf81f150fe7647a8f0cfae4
Gabrielferreiraav <gabriel.vianafr@gmail.com>
2022-12-10 14:40:09 -0300

    alteração main

--- maincliente.cpp
+++ maincliente.cpp
@@ -15,72 +15,100 @@ int main() {
     std::cout << std::endl;
 
 
-     switch(resposta) {
-         
-         case 1: {
+    switch (resposta) {
 
-            //std::string linha;
-            std::ifstream fin1("moletomCliente.txt");
-              if(fin1.is_open())
-                 while(getline(fin1,linha))
-                      std::cout << linha << std::endl;
-            std::cout << std::endl;
-            std::ifstream fin2("camisaCliente.txt");
-              if(fin2.is_open())
-                 while(getline(fin2,linha))
-                      std::cout << linha << std::endl;
-            std::cout << std::endl;          
-            std::ifstream fin3("canecaCliente.txt");
-              if(fin3.is_open())
-                 while(getline(fin3,linha))
-                      std::cout << linha << std::endl;
-            std::cout << std::endl;          
-            std::ifstream fin4("tiranteCliente.txt");
-              if(fin4.is_open())
-                 while(getline(fin4,linha))
-                      std::cout << linha << std::endl;          
-            std::cout << std::endl;
+    case 1: {
 
-            std::cout << "Gostaria de adquirir algum produto (Digite S para sim e N para nao)? ";
+        //std::string linha;
+        std::ifstream fin1("moletomCliente.txt");
+        if (fin1.is_open())
+            while (getline(fin1, linha))
+                std::cout << linha << std::endl;
+        std::cout << std::endl;
+        std::ifstream fin2("camisaCliente.txt");
+        if (fin2.is_open())
+            while (getline(fin2, linha))
+                std::cout << linha << std::endl;
+        std::cout << std::endl;
+        std::ifstream fin3("canecaCliente.txt");
+        if (fin3.is_open())
+            while (getline(fin3, linha))
+                std::cout << linha << std::endl;
+        std::cout << std::endl;
+        std::ifstream fin4("tiranteCliente.txt");
+        if (fin4.is_open())
+            while (getline(fin4, linha))
+                std::cout << linha << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "Gostaria de adquirir algum produto (Digite S para sim e N para nao)? ";
+        do {
+            try {
+                std::cin >> simounao;
+                if (simounao != 'N' && simounao != 'n' && simounao != 'S' && simounao != 's')
+                    throw "Resposta invalida! Digite S ou N: ";
+            }
+            catch (const char* msg) {
+                std::cout << msg << std::endl;
+                return true;
+            }
+        } while (erro);
+
+        if (simounao == 'N') {
+            std::cout << "Ah que pena, mas obrigado pela visita!";
+            return 0;
+        }
+        else {
+            while (true) {
+                std::cout << "Qual produto?\nDigite 1 - Moletom\nDigite 2 - Camisa\nDigite 3 - Caneca\nDigite 4 - Tirante\n";
                 do {
-                    try{
-                         std::cin >> simounao;
-                         if(simounao!='N' && simounao!='n' && simounao!='S' && simounao!='s')
-                             throw "Resposta invalida! Digite S ou N: "; 
-                        } catch(const char *msg) {
-                             std::cout << msg << std::endl;
-                             return true;
-                        }
-                } while(erro);
-
-             if(simounao=='N') {
-                 std::cout << "Ah que pena, mas obrigado pela visita!";
-                 return 0;
-             }
-             else {
-                while(true) {
-                    std::cout << "Qual produto?\nDigite 1 - Moletom\nDigite 2 - Camisa\nDigite 3 - Caneca\nDigite 4 - Tirante\n";
-                     do {
-                        try{
-                            std::cin >> qualproduto;
-                            if(qualproduto!=1 && qualproduto!=2 && qualproduto!=3 && qualproduto!=4)
-                                throw "Resposta invalida! Digite um numero que corresponda a um produto: "; 
-                            } catch(const char *msg) {
-                                std::cout << msg << std::endl;
-                                return true;
-                            }
-                     } while(erro);
-
-
-                    
-             }
-             }
+                    try {
+                        std::cin >> qualproduto;
+                        if (qualproduto != 1 && qualproduto != 2 && qualproduto != 3 && qualproduto != 4)
+                            throw "Resposta invalida! Digite um numero que corresponda a um produto: ";
+                    }
+                    catch (const char* msg) {
+                        std::cout << msg << std::endl;
+                        return true;
+                    }
+                } while (erro);
 
 
 
-            /* \nDigite 1 - Moletom\nDigite 2 - Camisa\nDigite 3 - Caneca\nDigite 4 - Tirante\n" << 
-             "Caso deseje sair do programa, digite 5";*/
-         }
+            }
+        }
+
+
+
+        /* \nDigite 1 - Moletom\nDigite 2 - Camisa\nDigite 3 - Caneca\nDigite 4 - Tirante\n" <<
+         "Caso deseje sair do programa, digite 5";*/
+    }
 
     }
+
+
+    //Leitura de um arquico para leitura de qtd de itens
+    /*string nome, cpf;
+    int qtdItens;
+    try {
+        ofstream arq("cadastro.txt");
+        if (arq.is_open()) {
+            cout << "Digite o nome: ";
+            getline(cin, nome);
+            cout << "Digite o CPF: ";
+            getline(cin, cpf);
+            cout << "Digite a quantidade de itens: ";
+            cin >> qtdItens;
+            arq << nome << "\n";
+            arq << cpf << "\n";
+            arq << qtdItens << "\n";
+            arq.close();
+            cout << "\nCadastro realizado com sucesso!\n";
+        }
+        else {
+            throw "Erro ao abrir o arquivo";
+        }
+    }
+    catch (const char* msg) {
+}*/
 }
